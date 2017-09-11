e49a3e6388145cb35ab61c27ea1d72705da2cc58
Mac <mac@Macde-MacBook-Air.local>
2017-09-11 22:36:35 +0800

    complete AddInfoViewController

--- Blind_Security_Info/ThirdViewController.swift
+++ Blind_Security_Info/ThirdViewController.swift
@@ -2,7 +2,7 @@
 //  ThirdViewController.swift
 //  Blind_Security_Info
 //
-//  Created by Nick on 05/09/2017.
+//  Created by Nick on 07/09/2017.
 //  Copyright © 2017 Nick. All rights reserved.
 //
 
@@ -10,10 +10,6 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
-    convenience init() {
-        self.init(nibName: "ThirdViewController1", bundle: nil)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
