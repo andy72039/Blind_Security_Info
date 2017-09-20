aea200cd1a7efa9a1aaf2cd9080d5ecd178e6a5a
Mac <mac@Macde-MacBook-Air.local>
2017-09-20 21:15:03 +0800

    simply design ThirdViewController for Settings

--- Blind_Security_Info/ThirdViewController.swift
+++ Blind_Security_Info/ThirdViewController.swift
@@ -9,27 +9,104 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-
+    var headerView: UIView! = UIView()
+    var titleLabel: UILabel! = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func updateViewConstraints() {
+        headerViewConstraints()
+        titleLabelConstraints()
+        super.updateViewConstraints()
     }
-    */
-
+    
+    func headerViewConstraints() {
+        NSLayoutConstraint(
+            item: headerView,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerX,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+        
+        NSLayoutConstraint(
+            item: headerView,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .width,
+            multiplier: 1.0,
+            constant: 0.0
+            )
+            .isActive = true
+        
+        NSLayoutConstraint(
+            item: headerView,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .top,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+        NSLayoutConstraint(
+            item: headerView,
+            attribute: .height,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .height,
+            multiplier: 0.2,
+            constant: 0.0)
+            .isActive = true
+    }
+    
+    func titleLabelConstraints() {
+        NSLayoutConstraint(
+            item: titleLabel,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: headerView,
+            attribute: .centerX,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+        
+        NSLayoutConstraint(
+            item: titleLabel,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: headerView,
+            attribute: .width,
+            multiplier: 0.6,
+            constant: 0.0)
+            .isActive = true
+        
+        NSLayoutConstraint(
+            item: titleLabel,
+            attribute: .centerY,
+            relatedBy: .equal,
+            toItem: headerView,
+            attribute: .centerY,
+            multiplier: 1.0,
+            constant: 0.0)
+            .isActive = true
+    }
+    
+    
+    func setupView() {
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.backgroundColor = UIColor.red
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Settings"
+        titleLabel.textAlignment = .center
+        
+        self.view.addSubview(headerView)
+        headerView.addSubview(titleLabel)
+        self.view.setNeedsUpdateConstraints()
+    }
 }
